563a51d5741b20a9b2e20c74ed170254c2329001
RamyaVVSS <vvssramya@gmail.com>
2021-05-21 11:09:49 +0530

    Initial Commit

--- src/com/capgemini/model/Address.java
+++ src/com/capgemini/model/Address.java
@@ -8,4 +8,38 @@ public class Address {
 	private String pin;
 	// this is address..
 	// Constructors, Getter & Setter method
+	public Address() {
+		
+	}
+	public Address(int addressId, String city, String state, String pin) {
+		this.addressId = addressId;
+		this.city = city;
+		this.state = state;
+		this.pin = pin;
+	}
+	public int getAddressId() {
+		return addressId;
+	}
+	public void setAddressId(int addressId) {
+		this.addressId = addressId;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	public String getPin() {
+		return pin;
+	}
+	public void setPin(String pin) {
+		this.pin = pin;
+	}
+	
 }
